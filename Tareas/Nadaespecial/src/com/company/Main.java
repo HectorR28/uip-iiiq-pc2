f9024dcad57736b98d8735a572e2526f8f622118
HectorR28 <31984729+HectorR28@users.noreply.github.com>
2017-09-22 21:29:03 -0500

    Primer programa (Prueba)
    Nadaespecial (Semana 3)

---
+++ Tareas/Nadaespecial/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	System.out.println("Bienvenidos a Java!");
+
+    //ENTEROS
+        byte a;
+        short b;
+        int c = 27;
+        long d;
+    //DECIMALES
+        float e;
+        double f = 0.0;
+
+    //CARACTERES
+        char g = 'x';
+        String i = "x";
+
+    //BOLEANO
+        boolean vivo = true;
+        vivo = false;
+
+    }
+}
